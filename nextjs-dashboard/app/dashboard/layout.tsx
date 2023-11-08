f39892bb41e0e1d4408e18743f42f3c2bf302257
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-11-08 18:18:30 +0100

    Build: next.js course chapter 8/16

--- nextjs-dashboard/app/dashboard/layout.tsx
+++ nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,4 +1,4 @@
-import SideNav from '@/app/ui/dashboard/sidenav'
+import SideNav from '@/app/ui/dashboard/sidenav';
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,4 +9,4 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   );
-}
+  }
